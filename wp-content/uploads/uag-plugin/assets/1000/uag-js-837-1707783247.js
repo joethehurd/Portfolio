3be088f1cea3ec57c4dbe458da7010aac362d065
joethehurd <josephmartinhurd@gmail.com>
2024-02-12 19:34:06 -0500

    v1.0.5

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-837-1707783247.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-0b3f53d2 .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-0b3f53d2 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-0b3f53d2 .swiper-button-next","prevEl":".uagb-block-0b3f53d2 .swiper-button-prev"}}	);
+});
+
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-6e10531a .uagb-swiper",
+		{"autoplay":{"delay":6000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":750,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-6e10531a .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-6e10531a .swiper-button-next","prevEl":".uagb-block-6e10531a .swiper-button-prev"}}	);
+});
+
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-94e23221 .uagb-swiper",
+		{"autoplay":{"delay":6000,"disableOnInteraction":true,"pauseOnMouseEnter":false,"stopOnLastSlide":false},"loop":true,"speed":750,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-94e23221 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-94e23221 .swiper-button-next","prevEl":".uagb-block-94e23221 .swiper-button-prev"}}	);
+});
+
+ })
---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-837-1707783479.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-36f0b1f9 .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-36f0b1f9 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-36f0b1f9 .swiper-button-next","prevEl":".uagb-block-36f0b1f9 .swiper-button-prev"}}	);
+});
+
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-3b0b4b77 .uagb-swiper",
+		{"autoplay":{"delay":6000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":750,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-3b0b4b77 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-3b0b4b77 .swiper-button-next","prevEl":".uagb-block-3b0b4b77 .swiper-button-prev"}}	);
+});
+
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-fe8951e9 .uagb-swiper",
+		{"autoplay":{"delay":6000,"disableOnInteraction":true,"pauseOnMouseEnter":false,"stopOnLastSlide":false},"loop":true,"speed":750,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-fe8951e9 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-fe8951e9 .swiper-button-next","prevEl":".uagb-block-fe8951e9 .swiper-button-prev"}}	);
+});
+
+ })
