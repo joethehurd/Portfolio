670e37be0d0fd396fed547d35f57643533607039
joethehurd <josephmartinhurd@gmail.com>
2024-03-14 16:26:55 -0400

    v1.0.6

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-837-1710446833.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-64c2c978 .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-64c2c978 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-64c2c978 .swiper-button-next","prevEl":".uagb-block-64c2c978 .swiper-button-prev"}}	);
+});
+
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-89d9b19a .uagb-swiper",
+		{"autoplay":{"delay":6000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":750,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-89d9b19a .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-89d9b19a .swiper-button-next","prevEl":".uagb-block-89d9b19a .swiper-button-prev"}}	);
+});
+
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-2974fff8 .uagb-swiper",
+		{"autoplay":{"delay":6000,"disableOnInteraction":true,"pauseOnMouseEnter":false,"stopOnLastSlide":false},"loop":true,"speed":750,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-2974fff8 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-2974fff8 .swiper-button-next","prevEl":".uagb-block-2974fff8 .swiper-button-prev"}}	);
+});
+
+ })
